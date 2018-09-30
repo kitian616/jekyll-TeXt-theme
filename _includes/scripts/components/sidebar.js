8cfae6707fb44223dd583e5edf5a3ec3923fe7af
kitian616 <kitian616@outlook.com>
2018-09-30 23:43:55 +0800

    feat: enhance page with sidebar

--- _includes/scripts/components/sidebar.js
+++ _includes/scripts/components/sidebar.js
@@ -1,14 +1,34 @@
 (function() {
   var SOURCES = window.TEXT_VARIABLES.sources;
+
   window.Lazyload.js(SOURCES.jquery, function() {
+    var $pageMask = $('.js-page-mask');
     var $pageRoot = $('.js-page-root');
+    var $pageMain = $('.js-page-main');
     var $sidebarShow = $('.js-sidebar-show');
     var $sidebarHide = $('.js-sidebar-hide');
+
+    var scrollTop;
+
+    function freeze(e) {
+      if (e.target === $pageMask[0]) {
+        e.preventDefault();
+      }
+    }
+    function stopBodyScrolling(bool) {
+      if (bool === true) {
+        document.addEventListener('touchmove', freeze, { passive: false });
+      } else {
+        document.removeEventListener('touchmove', freeze, { passive: false });
+      }
+    }
+
     $sidebarShow.on('click', function() {
-      $pageRoot.addClass('show-sidebar');
+      scrollTop = $(window).scrollTop();
+      stopBodyScrolling(true); $pageRoot.addClass('show-sidebar'); $pageMain.scrollTop(scrollTop);
     });
     $sidebarHide.on('click', function() {
-      $pageRoot.removeClass('show-sidebar');
+      stopBodyScrolling(false); $pageRoot.removeClass('show-sidebar'); $(window).scrollTop(scrollTop);
     });
   });
 })();
