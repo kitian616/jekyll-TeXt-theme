37bdea19cb3953752973fa86c0988c4f709dc563
Holon <holon.cn@gmail.com>
2021-11-25 21:08:18 +0800

    修改获取脚本路径方式

--- autoGit.sh
+++ autoGit.sh
@@ -1,5 +1,7 @@
 # 自动化推送脚本, 在 ~/.zshrc 里面设置脚本运行的 alias
-cd /Users/holon/Documents/GitHub/wholon.github.io
+# 获取脚本所在绝对路径
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd $SHELL_FOLDER
 read -p  "please input commit comments:" msg
 git add .
 git commit -m "$msg"
