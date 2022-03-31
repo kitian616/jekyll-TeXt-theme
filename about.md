e05195afc3ac56877f364d78fc23014c2e7c1d1a
Holon <holon.cn@gmail.com>
2022-03-31 13:38:12 +0800

    更新文章

--- about.md
+++ about.md
@@ -23,8 +23,7 @@ titles:
   fr-FR   : *FR
   fr-LU   : *FR
   # @end locale config
-key: page-about
-title: 
+title: 关于
 ---
 
 
--- tools/diff.sh
+++
@@ -1,5 +0,0 @@
-Diffmerge ./_config.yml ./docs/_config.yml ./docs/_config.dev.yml
-Diffmerge ./_config.yml ./test/_config.yml
-
-Diffmerge ./_data/ ./docs/_data/
-Diffmerge ./_data/ ./test/_data
